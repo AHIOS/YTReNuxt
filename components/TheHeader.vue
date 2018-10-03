8b871dee11b42122e887f068f47a6cca246bda54
AHIOS <ahios.dev@gmail.com>
2018-10-03 13:13:57 +0200

    * refactor

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -13,29 +13,7 @@
         <span class="title">YTReNuxt</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-menu :nudge-width="100">
-        <v-list-tile avatar slot="activator">
-            <v-list-tile-avatar>
-              <img :src="`${user.photoUrl}`" alt="">
-            </v-list-tile-avatar>
-            <v-list-tile-title v-text="user.displayName"></v-list-tile-title>
-            <v-icon dark>arrow_drop_down</v-icon>
-          </v-list-tile>
-        <v-list>
-          <v-list-tile @click="navTo('/settings')">
-            <v-list-tile-action>
-              <v-icon>settings</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-title>Settings</v-list-tile-title>
-          </v-list-tile>
-          <v-list-tile @click="navTo('/logout')">
-            <v-list-tile-action>
-              <v-icon>logout</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-title>Logout</v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-menu>
+      <user-avatar :user="user"></user-avatar>
     </v-toolbar>
     <the-drawer 
         :isOpen="this.drawer"
@@ -46,10 +24,12 @@
 
 <script>
 import TheDrawer from '@/components/TheDrawer';
+import UserAvatar from '@/components/UserAvatar';
 import { mapActions, mapGetters } from 'vuex'
 export default {
     components: {
-      TheDrawer
+      TheDrawer,
+      UserAvatar
     },
     data: () => ({
       drawer: true,
---
+++ components/UserAvatar.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-menu :nudge-width="100" v-if="user.googleId">
+        <v-list-tile avatar slot="activator">
+            <v-list-tile-avatar>
+              <img :src="user.photoUrl" alt="">
+            </v-list-tile-avatar>
+            <v-list-tile-title v-text="user.displayName"></v-list-tile-title>
+            <v-icon dark>arrow_drop_down</v-icon>
+          </v-list-tile>
+        <v-list>
+          <v-list-tile @click="navTo('/settings')">
+            <v-list-tile-action>
+              <v-icon>settings</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile>
+          <v-list-tile @click="navTo('/logout')">
+            <v-list-tile-action>
+              <v-icon>logout</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-title>Logout</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+      <v-toolbar-items v-else>
+      <v-btn flat :to="{ name: 'login' }">Login</v-btn>
+    </v-toolbar-items>
+</template>
+
+<script>
+export default {
+    props: ['user'],
+}
+</script>
