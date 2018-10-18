085ed9692ff57d9ae7fd34a2a111c34eaaa77a83
AHIOS <ahios.dev@gmail.com>
2018-10-18 17:07:41 +0200

    * new clean store

---
+++ store/feathers-client.js
@@ -0,0 +1,18 @@
+import { CookieStorage } from 'cookie-storage'
+import feathers from '@feathersjs/feathers'
+import socketio from '@feathersjs/socketio-client'
+import auth from '@feathersjs/authentication-client'
+import io from 'socket.io-client'
+
+export const host = process.env.API_HOST || 'http://localhost:3030'
+console.log('wsHost: ' + host)
+
+const socket = io(host, {
+    transports: ['websocket']
+  })
+
+  const feathersClient = feathers()
+    .configure(socketio(socket))
+    .configure(auth({ storage: new CookieStorage() }))
+
+export default feathersClient
